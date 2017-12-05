85c40b2b7210adbf28a1e4f62c220acc0424e1c6
Shalvah Adebayo <shalvah.adebayo@konga.com>
2017-12-05 13:10:43 +0100

    Add seed for polls

---
+++ bin/seed.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+require('mongoose').connect('mongodb://localhost/pollser');
+
+const topics = [
+    "Should dogs be allowed to fly?",
+    "Should doors be shut at night?",
+    "Should developers use IDEs?"
+];
+let Poll = require('../models/poll');
+
+// empty the collection first
+Poll.remove({})
+    .then(() => {
+        let polls = [];
+        for (let i = 0; i < 5; i++) {
+            polls.push({
+                topic: topics[i],
+                options: [
+                    {
+                        value: "Yes",
+                        votes: Math.round(Math.random() * 20)
+                    },
+                    {
+                        value: "No",
+                        votes: Math.round(Math.random() * 20)
+                    },
+                    {
+                        value: "I really don't care",
+                        votes: Math.round(Math.random() * 20)
+                    }
+                ]
+            });
+        }
+        return Poll.create(polls);
+    })
+    .then(() => {
+        process.exit();
+    })
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    });
