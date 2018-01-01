553e2b184189bb10d77a9c72fb32fd53e31c5e3e
Shalvah Adebayo <shalvah.adebayo@konga.com>
2018-01-01 22:18:11 +0100

    Add voting logic

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,17 @@ let Poll = require('./../models/poll');
 
 router.get('/', (req, res, next) => {
     Poll.find().exec((err, polls) => {
-        res.render('index', { polls: polls });
+        res.render('index', { polls });
     });
+});
+
+router.post('/:pollId/vote', (req, res, next) => {
+    const choice = req.body.choice;
+    const identifier = `choices.${choice}.votes`;
+    Poll.update({_id: req.params.pollId}, {$inc: {[identifier]: 1}}, {}, (err, numberAffected) => {
+        res.send('');
+    });
+
 });
 
 module.exports = router;
