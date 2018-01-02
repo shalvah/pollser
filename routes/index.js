398fa5b58404eca92825cac277e459371da34222
Shalvah Adebayo <shalvah.adebayo@konga.com>
2018-01-02 15:22:46 +0100

    Add pusher event broadcasting

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,17 @@ router.post('/:pollId/vote', (req, res, next) => {
     const choice = req.body.choice;
     const identifier = `choices.${choice}.votes`;
     Poll.update({_id: req.params.pollId}, {$inc: {[identifier]: 1}}, {}, (err, numberAffected) => {
+        let Pusher = require('pusher');
+        let pusher = new Pusher({
+            appId: process.env.PUSHER_APP_ID,
+            key: process.env.PUSHER_APP_KEY,
+            secret: process.env.PUSHER_APP_SECRET,
+            cluster: 'eu'
+        });
+
+        let payload = { pollId: req.params.pollId, choice: choice };
+        pusher.trigger('poll-events', 'vote', payload, req.body.socketId);
+
         res.send('');
     });
 
